774004bc4560e10a300742df2f6978b83ddd85cf
nicat827 <97800703+nicat827@users.noreply.github.com>
2022-03-15 18:13:00 +0400

    Update views.py

--- views.py
+++ views.py
@@ -1,29 +1,30 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 from .models import Message
 from django.http import HttpResponse, JsonResponse
 
-def ru_version(request):
+#def ru_version(request):
 	return render(request, 'main/ru_version.html')
 
-def en_version(request):
+#def en_version(request):
 	return render(request, 'main/en_version.html')
 
-def deposit(request):
+#def deposit(request):
 	return render(request, 'main/deposit.html')
 
-def send(request):
-	if  request.POST.get('message').strip() != '':
-		message = request.POST.get('message')
-		new_message = Message.objects.create(value=message)
-		return HttpResponse('Message sent successfully')
-	else:
-		b = request.POST.get('message')
-
-		return render(request, 'main/ru_version.html')
+#def send(request):
+#	if  request.POST.get('message').strip() != '':
+#		message = request.POST.get('message')
+#		new_message = Message.objects.create(value=message)
+#		return HttpResponse('Message sent successfully')
+#	else:
+#		b = request.POST.get('message')
+#
+#		return render(request, 'main/ru_version.html')
 
 
 
 def getMessages(request):
-	messages = Message.objects.all()
+	messages = Message.objects.order_by('-id')[:15]
+	#ТУТ НУЖНО РЕВЕРСНУТЬ И ПОЛУЧИТЬ ОБРАТНЫЙ СПИСОК
 	return JsonResponse({"messages" : list(messages.values())})
